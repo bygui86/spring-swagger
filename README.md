2f4d8dd3cd137f4d7c45342bcb20e59422c5dc89
Matteo Baiguini <baiguini.matteo@gmail.com>
2018-07-25 17:51:50 +0200

    First commit from start.spring.io

---
+++ README.md
@@ -0,0 +1,9 @@
+# Spring Swagger
+
+## topics
+* spring boot
+* swagger
+
+
+## links
+http://www.baeldung.com/swagger-2-documentation-for-spring-rest-api
---
+++ src/main/java/com/rabbit/swaggersample/SwaggerSampleApplication.java
@@ -0,0 +1,12 @@
+package com.rabbit.swaggersample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerSampleApplication.class, args);
+	}
+}
